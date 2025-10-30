15d3cba4157100a10006bd5f6b5d5b20bc25a0ee
Adarsh Sachan <2k23.cs2312635@gmail.com>
2025-10-30 15:36:11 +0530

    get movie data testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Home from './Components/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { use } from "react";
 import { Search } from "lucide-react";
+import axios from "axios";
+import { useState } from "react";
+
 
 const Navbar = () => {
+const omdAPI_KEY = import.meta.env.VITE_omdAPI_KEY;
+console.log(omdAPI_KEY);
+  const [movieName, setMovieName] = useState("");
+  async function getMovieName() {
+    const url = axios.get(
+      `https://www.omdbapi.com/?s=avengers&apikey=${omdAPI_KEY}`
+    );
+    const response = await url;
+    console.log(response.data);
+  }
+
   return (
     <div>
       <div className="bg-yellow-300 h-12 flex items-start text-2xl font-bold text-gray-800">
@@ -11,6 +25,13 @@ const Navbar = () => {
           name="search"
           className="placeholder:text-gray-500 placeholder:italic"
         />
+        <button onClick={
+          () => {
+            getMovieName();
+          }
+        }>
+          click to get hehe
+        </button>
       </div>
     </div>
   );
